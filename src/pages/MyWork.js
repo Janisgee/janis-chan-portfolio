f613a1bad9eac95b3da360b7f5093d8c69652719
janisgee <janisgeegee@gmail.com>
2024-01-18 11:50:44 +0800

    Added mywork list and animation

--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -1,9 +1,23 @@
 import HeadingBar from './HeadingBar';
+import './MyWork.css';
 
 export default function MyWork() {
   return (
     <div className='myWork side-wrapper'>
       <HeadingBar />
+
+      <ul className='projects to-right animation-common'>
+        <li>Money Tracker Project</li>
+        <li>Worldwise Project</li>
+        <li>Weather Project</li>
+        <li>Forkify Recipe Project</li>
+        <li>Pig Game</li>
+        <li>Tic Tac Toe Game</li>
+        <li>Dictionary Project</li>
+        <li>Shopping List Project</li>
+        <li>Movie Rating Project</li>
+        <li>React Quiz Project</li>
+      </ul>
     </div>
   );
 }
