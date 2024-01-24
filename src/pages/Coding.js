43014e5fa053a553abb2e6442c953cc0c9feb876
janisgee <janisgeegee@gmail.com>
2024-01-24 18:01:46 +0800

    Added move left animation for coding page title

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -1,10 +1,26 @@
-import './About.css';
+import './Coding.css';
 import HeadingBar from './utilities/HeadingBar.js';
 
 export default function Coding() {
+  function handleEnterClick(e) {
+    e.preventDefault();
+
+    console.log('Enter');
+    e.target.parentElement.classList.add('to-left');
+    setTimeout(() => {
+      console.log('Delayed for 4 second.');
+      e.target.parentElement.classList.add('hidden');
+    }, '4000');
+  }
   return (
     <div className='coding side-wrapper'>
       <HeadingBar />
+      <div className='to-right animation-common'>
+        <div className='codingbackground'>Skill in Coding</div>
+        <div className='enterSign codingbackground' onClick={handleEnterClick}>
+          ENTER►
+        </div>
+      </div>
     </div>
   );
 }
